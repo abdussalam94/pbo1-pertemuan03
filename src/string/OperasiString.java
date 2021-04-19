34a52a3a65ebd2bcaac60a40c4f28fd684b21900
mirzayogy <mirza.yogy@gmail.com>
2021-04-20 04:10:29 +0800

    toUppercase untuk ... toLowercase untuk ... substring(5) untuk ... substring(0,4) untuk ...

--- src/string/OperasiString.java
+++ src/string/OperasiString.java
@@ -6,8 +6,13 @@ public class OperasiString {
 
         System.out.println(kota);
 
-        char[] uniskaChar = { 'U', 'N', 'S', 'I', 'K', 'A'};
+        char[] uniskaChar = { 'U','N','S','I','K','A' };
         String uniskaString = new String(uniskaChar);
         System.out.println(uniskaString);
+
+        System.out.println(kota.toUpperCase());
+        System.out.println(kota.toLowerCase());
+        System.out.println(kota.substring(5));
+        System.out.println(kota.substring(0,4));
     }
 }
